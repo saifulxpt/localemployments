f4652e12ebbd994907f836014879baaddf86d332
Saiful Islam <saiful@assessmentbd.com>
2026-08-14 01:19:07 +0600

    Fix public/index.php paths for cPanel shared hosting deployment

--- public/index.php
+++ public/index.php
@@ -5,21 +5,13 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__.'/../localemployments/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Require Custom Helpers manually for shared hosting
-if (file_exists(__DIR__.'/../app/Helpers/helpers.php')) {
-    require_once __DIR__.'/../app/Helpers/helpers.php';
-}
+require __DIR__.'/../localemployments/vendor/autoload.php';
 
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
-
-// Bootstrap Laravel and handle the request...
 /** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/../localemployments/bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
